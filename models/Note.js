bbdb4b43d3156b64ed4049b1effdc9722c569cf3
omkardhenge <133488399+omkardhenge@users.noreply.github.com>
2025-06-13 21:08:19 +0530

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true }
+    title: {
+         type: String, required: true 
+    },
+    content: { 
+        type: String, required: true 
+    },
+    notePriority: {
+         type: String, required: true 
+    }  // ✅ Add this line
+    
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
